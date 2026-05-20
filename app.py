eaacf76c6b0a78d7b676a5ce3ee22e9e3be5c534
marian2566 <mbalderrama@fi.uba.ar>
2026-05-20 17:30:00 -0300

    subiendo todo

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# 1. Ruta para la página principal (Inicio)
+@app.route("/")
+def index():
+    return render_template('indexreservas.html')
+
+# 2. Ruta para la página de reseñas
+@app.route("/reseñas")
+def reseñas():
+    return render_template('adminresenas.html')
+# 3. Ruta para la página de administración de reservas
+@app.route("/admin-reservas")
+def admin_reservas():
+    return render_template('adminreservas.html')
+@app.route("/sala-privada")
+def sala_privada():
+    return render_template('salaprivada.html')
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000, debug=True)

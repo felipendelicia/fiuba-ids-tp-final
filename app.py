36626ae91085d1fab31221e8311073b353175663
Gabriel_Alexander <gsaavedrag@fi.uba.ar>
2026-05-20 18:21:39 -0300

    "feat: agregados templates de servicios y contacto con bootstrap"

--- app.py
+++ app.py
@@ -1,30 +1,24 @@
-import os
-
-from dotenv import load_dotenv
-from flask import Flask
-
-
-from routes.account_routes import account_bp
-from routes.authentication_routes import authentication_bp
-from routes.gamemodes_routes import gamemodes_bp
-from routes.reviews_routes import reviews_bp
-from routes.reservations_routes import reservations_bp
-from routes.dashboard_routes import dashboard_bp
-from routes.maps_routes import maps_bp
-from routes.equipmentkit_routes import equipmentkit_bp
-
-
-
-load_dotenv()
+rom flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Para modificar en el desarrollo:
+# pantalla de Servicios
+@app.route('/servicios')
+def servicios():
+    # lo que devolvería tu base de datos MySQL
+    modos_prueba = [
+        {"id": 1, "name": "Captura la Bandera", "description": "Llevá la bandera enemiga a tu base.", "duration": 45, "number_players": 20, "price": 4500.50},
+        {"id": 2, "name": "Team Deathmatch", "description": "Eliminá al equipo contrario.", "duration": 30, "number_players": 16, "price": 3000.00}
+    ]
+    return render_template('services.html', gamemodes=modos_prueba)
+
+# pantalla de Contacto
+@app.route('/contacto')
+def contacto():
+    return render_template('contact.html')
+
 if __name__ == '__main__':
-    if os.getenv('ENV') == 'dev':
-        app.run(debug= True, port=os.getenv('FLASK_PORT'))
-    else:
-        app.run(debug= False, host='0.0.0.0', port=os.getenv('FLASK_PORT'))
+    app.run(debug=True)
 
 
 

df84262986aae25649c8a6d2aa659c6826e8e113
felipendelicia <delicia4581@gmail.com>
2026-05-20 15:08:06 -0300

    fix(schema): Sintaxis errors fixed.

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS airsoftdb;
 
 USE airsoftdb;
 
-CREATE TABLE IF NOT EXISTS Accounts {
+CREATE TABLE IF NOT EXISTS Accounts (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(100) NOT NULL,
 	username VARCHAR(100) NOT NULL,
@@ -17,13 +17,13 @@ CREATE TABLE IF NOT EXISTS Accounts {
 	last_reservation DATE,
 	is_active BOOLEAN DEFAULT TRUE,
 	elo INT
-};
+);
 
 
-CREATE TABLE IF NOT EXISTS Reservations {
+CREATE TABLE IF NOT EXISTS Reservations (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	account_id INT NOT NULL,
-	game_mode_id INT NOT NUL,
+	game_mode_id INT NOT NULL,
 	map_id INT NOT NULL,
 	created_at DATETIME,
 	equipment_kit_id INT NOT NULL,
@@ -33,51 +33,48 @@ CREATE TABLE IF NOT EXISTS Reservations {
 	end_time DATE,
 	is_public BOOLEAN,
 	canceled BOOLEAN DEFAULT FALSE,
-	cancelation_reason VARCHAR
-};
+	cancelation_reason VARCHAR(500)
+);
 
 
-CREATE TABLE IF NOT EXISTS RegisteredPlayers {
+CREATE TABLE IF NOT EXISTS RegisteredPlayers (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	reservation_id INT NOT NULL,
 	account_id INT NOT NULL,
 	created_at DATE
-};
+);
 
 
-CREATE TABLE IF NOT EXISTS EquipmentKit {
+CREATE TABLE IF NOT EXISTS EquipmentKit (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(100) NOT NULL,
 	brand VARCHAR(100),
 	price FLOAT
-};
+);
 
 
-CREATE TABLE IF NOT EXISTS Maps {
+CREATE TABLE IF NOT EXISTS Maps (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	image_url VARCHAR(100),
 	name VARCHAR(100),
 	description VARCHAR(900)
-};
+);
 
 
-CREATE TABLE IF NOT EXISTS Review {
+CREATE TABLE IF NOT EXISTS Review (
 	id INT AUTO_INCREMENT PRIMARY KEY,
-	stars INT NOT NULL CHECK (stars BETWEEN 1 AND 5)
-	body_review VARCHAR(900)
+	stars INT NOT NULL CHECK (stars BETWEEN 1 AND 5),
+	body_review VARCHAR(900),
 	map_id INT NOT NULL,
 	created_at DATE,
 	approved BOOLEAN
-};
+);
 
 
-CREATE TABLE IF NOT EXISTS GameModes {
+CREATE TABLE IF NOT EXISTS GameModes (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(75) NOT NULL,
 	duration ENUM('30', '60', '90', '120') NOT NULL,
 	players INT NOT NULL,
 	updated_at DATE
-};
-
-
-
+);

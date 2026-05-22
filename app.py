15af3692e53d1d01d941f6452abbe88a428b1613
ronny <ronnymtorrez@gmail.com>
2026-05-22 09:38:08 -0300

    Referenciación de rutas(contactos, servicios) en el index, ordenamiento de rutas en el app.py

--- app.py
+++ app.py
@@ -2,56 +2,67 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+#1. Ruta para la página de Inicio
 @app.route("/")
 def index():
     return render_template('index.html')
 
-@app.route("/campos")
-def campos():
-    return render_template('campos.html')
-
-
-# 1. Ruta para la página (reservas)
-@app.route("/indexresevas")
-def indexreservas():
-    return render_template('indexreservas.html')
-
-# 2. Ruta para la página de reseñas
-@app.route("/reseñas")
-def reseñas():
-    return render_template('adminresenas.html')
-# 3. Ruta para la página de administración de reservas
-@app.route("/admin-reservas")
-def admin_reservas():
-    return render_template('adminreservas.html')
-@app.route("/sala-privada")
-def sala_privada():
-    return render_template('salaprivada.html')
-
+# 2. Ruta para la página de Iniciar Sesion
 @app.route("/login")
 def login():
     return render_template('login.html')
 
+# 3. Ruta para la página de Registrar Nuevo Usuario
 @app.route('/register')
 def register():
   return render_template('register.html')
 
+# 4. Ruta para la pagína de Recuperación Contraseña de Usuario
 @app.route('/password')
 def password():
   return render_template('password.html')
 
+# 5. Ruta para la página de Campos de Juegos
+@app.route("/campos")
+def campos():
+    return render_template('campos.html')
+
+# 6. Ruta para la página Reservas de Campos
+@app.route("/reservas")
+def reservas():
+    return render_template('reservas.html')
+
+# 7. Ruta para la página de Reseñas
+@app.route("/reseñas")
+def reseñas():
+    return render_template('adminresenas.html')
+
+# 8. Ruta para la página de administración de reservas
+@app.route("/admin-reservas")
+def admin_reservas():
+    return render_template('adminreservas.html')
+
+# 9. Ruta para la página de Sala Privada
+@app.route("/sala-privada")
+def sala_privada():
+    return render_template('salaprivada.html')
+
+# 10. Ruta para la página de Dashboard
 @app.route('/dashboard')
 def dasboard():
   return render_template('dashboard.html')
 
+# 11. Ruta para la página de Lobby para Salas Públicas
 @app.route("/lobby-publicas")
 def lobby_publicas():
     return render_template('lobbypublicas.html')
 
+# 12. Ruta para la página de Reserva de Sala Pública
 @app.route("/sala-publica")
 def sala_publica():
     return render_template('salaspublicas.html')
 
+# 13. Ruta para la página de Servicios
 @app.route('/servicios')
 def servicios():
     servicios_db = [
@@ -60,6 +71,7 @@ def servicios():
     ]
     return render_template('servicios.html', servicios=servicios_db)
 
+# 14. Ruta para la página de Contacto
 @app.route('/contacto', methods=['GET', 'POST'])
 def contacto():
     if request.method == 'POST':
@@ -69,6 +81,7 @@ def contacto():
         return redirect(url_for('contacto'))
     return render_template('contacto.html')
 
+# 15. Ruta para la página de Administrador de Servicios
 @app.route('/adminservicios', methods=['GET', 'POST'])
 def admin_servicios():
     servicios_db = [
@@ -83,6 +96,7 @@ def admin_servicios():
         return render_template('adminservicios.html', servicios=servicios_db)
     return render_template('adminservicios.html', servicios=servicios_db)
 
+# 16. Ruta para la página de Equipamientos
 @app.route('/equipamiento', methods=['GET', 'POST'])
 def equipamiento():
     equipamiento_db = [

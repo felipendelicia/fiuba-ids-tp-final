be4ffec09b3a5463aed9744a9d1436c814ce26d4
Kiliano <kolivera@fi.uba.ar>
2026-05-26 18:18:52 -0300

    feat(gamemodes_controllers): implementar listar_game_modes

--- controllers/gamemodes_controllers.py
+++ controllers/gamemodes_controllers.py
@@ -1,10 +1,17 @@
 from flask import request, jsonify
 from db import execute
 from errors import ERRORS
-
+from services.gamemodes_services import (
+    listar_game_modes as service_listar_game_modes,
+    crear_game_mode as service_crear_game_mode,
+    reemplazar_game_mode as service_reemplazar_game_mode,
+)
 
 def listar_game_modes():
-    pass
+    gamemodes = service_listar_game_modes()
+    if gamemodes is None:
+        return ERRORS['UNKNOWN ERROR']
+    return jsonify({'gamemodes': gamemodes}), 200
 
 
 def crear_game_mode(id):

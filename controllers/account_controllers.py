9176f44a0cda6478073c6d31c744902b9f6050cf
ronny <ronnymtorrez@gmail.com>
2026-05-27 13:54:01 -0300

    feat(account): Implementación de actualizar_usuario por id

--- controllers/account_controllers.py
+++ controllers/account_controllers.py
@@ -4,6 +4,8 @@ from errors import ERRORS
 from helpers import build_links
 from services.account_services import listar_usuarios as listar_usuarios_service
 from services.account_services import obtener_cuenta as obtener_cuenta_service
+from services.account_services import actualizar_usuario as actualizar_usuario_service
+
 
 
 def listar_usuarios():
@@ -19,7 +21,21 @@ def obtener_cuenta(id):
     return jsonify({'Cuenta': usuario_id}), 200
 
 def actualizar_usuario(id):
-    pass
+    data = request.get_json()
+    if ('username' not in data) or ('email' not in data) or ('password' not in data) or ('phone' not in data) or ('elo' not in data) or ('is_active' not in data):
+        return ERRORS['MISSING_REQUIRED_FIELDS']('Los campos son: username, email, password, phone, elo, is_active')
+    username = data['username']
+    email = data['email']
+    password = data['password']
+    phone = data['phone']
+    elo = data['elo']
+    is_active = data['is_active']
+    estado = actualizar_usuario_service(id, username, email, password, phone, elo, is_active)
+    if estado is None:
+        return ERRORS['NOT_FOUND']("Usuario con el id no encontrado"), 404
+    if estado == False:
+        return ERRORS['UNKNOWN_ERROR']("Error al actualizar usuario"), 500
+    return '', 204
 
 def listar_reservas(id):
     #Paginación
--- services/account_services.py
+++ services/account_services.py
@@ -14,9 +14,14 @@ def obtener_cuenta(id):
         return None
     return cuenta_id
 
-def actualizar_usuario(id):
-    pass
-
+def actualizar_usuario(id, username, email, password, phone, elo, is_active):
+    cuenta_id = execute(f'SELECT * FROM Accounts WHERE id = {id}')
+    if not cuenta_id:
+        return None
+    cuenta_actualizada = execute(f"UPDATE Accounts SET username = '{username}', email = '{email}', password = '{password}', phone = '{phone}', elo = '{elo}', is_active = {is_active} WHERE id = '{id}'")
+    if cuenta_actualizada == False:
+        return False
+    return cuenta_actualizada
 
 def listar_reservas(id):
     pass
